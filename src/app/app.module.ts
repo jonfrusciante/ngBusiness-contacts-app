b401faf755887b5e2d937dc8b5cc1bb034e94217
Stefan <stefantrajkovic777@gmail.com>
2016-11-21 10:55:40 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAvCyxyv16SWvNQWN0LUSEZirV2iVXxLO0",
+  authDomain: "stefan-contacts.firebaseapp.com",
+  databaseURL: "https://stefan-contacts.firebaseio.com",
+  storageBucket: "stefan-contacts.appspot.com"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +19,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
